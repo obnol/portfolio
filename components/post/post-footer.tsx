51f3ac6f9461dbaf54336490cf08f390dec8cd3b
Longbo <longboqiu@hotmail.com>
2024-05-30 19:28:50 +0200

    feat: upgrade packages

--- components/post/post-footer.tsx
+++ components/post/post-footer.tsx
@@ -1,30 +1,30 @@
-'use client';
+"use client";
 
-import { ExternalLinkIcon } from '@radix-ui/react-icons';
-import { buttonVariants } from '../ui/button';
-import Link from 'next/link';
-import { cn } from '@/lib/utils';
-import { Post } from '@/lib/types';
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { buttonVariants } from "../ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Post } from "@/lib/types";
 
-type PostFooterProps = Pick<Post, 'link'>;
+type PostFooterProps = Pick<Post, "link">;
 
 export default function PostFooter({ link }: PostFooterProps): JSX.Element {
   return (
-    <div className='flex items-center justify-end'>
+    <div className="flex items-center justify-end">
       <Link
         href={link}
-        target='_blank'
-        rel='noreferrer'
+        target="_blank"
+        rel="noreferrer"
         className={cn(
           buttonVariants({
-            size: 'icon',
-            variant: 'ghost',
+            size: "icon",
+            variant: "ghost",
           }),
-          'cursor-alias'
+          "cursor-alias"
         )}
-        aria-label='View post'
+        aria-label="View post"
       >
-        <ExternalLinkIcon className='h-4 w-4' aria-hidden='true' />
+        <ExternalLinkIcon className="h-4 w-4" aria-hidden="true" />
       </Link>
     </div>
   );
--- lib/feed-data.ts
+++ lib/feed-data.ts
@@ -1,18 +1,18 @@
-import { Post } from './types';
+import { Post } from "./types";
 
 export const feedPosts: Post[] = [
   {
-    image: '/bulkbuy-icon.webp',
-    entity: 'Bulkbuy',
-    description: 'Your #1 source for local sneakers reselling. Easily generate your WTB and redefine your bulkbuy',
-    link: 'https://bulkbuy.io',
-    date: new Date('2023-11-14'),
+    image: "/bulkbuy-icon.webp",
+    entity: "Bulkbuy",
+    description: "Your #1 source for local sneakers reselling. Easily generate your WTB and redefine your bulkbuy",
+    link: "https://bulkbuy.io",
+    date: new Date("2023-11-14"),
   },
   {
-    image: '/tracksvault-logo.png',
-    entity: 'Tracksvault',
-    description: 'Tracksvault shows your top tracks from Spotify! 🎵',
-    link: 'https://tracksvault.app',
-    date: new Date('2023-07-04'),
+    image: "/tracksvault-logo.png",
+    entity: "Tracksvault",
+    description: "Tracksvault shows your top tracks from Spotify! 🎵",
+    link: "https://tracksvault.app",
+    date: new Date("2023-07-04"),
   },
 ];
