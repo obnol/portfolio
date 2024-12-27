9b7212a37115b456320af47eecb92f252802dde4
Longbo <longboqiu@hotmail.com>
2024-12-28 00:31:57 +0100

    fix: go back lowercase

--- app/go-back-button.tsx
+++ app/go-back-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function GoBackButton() {
   return (
     <Link href="/" passHref>
-      <p className="text-blue-600 hover:underline">← Go back</p>
+      <p className="text-blue-600 hover:underline">← go back</p>
     </Link>
   );
 }
