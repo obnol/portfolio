aac19ea9750a577fff675e2418241eacb1a37f79
Longbo <longboqiu@hotmail.com>
2025-11-29 09:35:34 +0100

    feat: updates

--- app/introduction.tsx
+++ app/introduction.tsx
@@ -2,10 +2,11 @@ export function Introduction() {
   return (
     <div className="space-y-4">
       <p>
-        hey, i&apos;m <span className="italic font-bold">obnol</span> — a software engineer based in barcelona, spain
+        hey, i&apos;m <span className="italic font-bold">obnol</span>, a software engineer based in barcelona, spain
       </p>
+
       <p>
-        passionate about <span className="font-bold italic">building software</span> that is simple, reliable, and delightful to use
+        i&apos;m currently working at <span className="font-bold italic">stripe</span>
       </p>
       <p>interested in frontend development and modern web technologies</p>
       <p>
